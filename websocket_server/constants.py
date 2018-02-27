5eb29ed675ab1295521331110f75de2e175b1b4c
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-02-27 22:42:10 +0100

    Reconsider constant alignment

--- websocket_server/constants.py
+++ websocket_server/constants.py
@@ -8,15 +8,15 @@ Various (numeric) constants.
 # Opcode constants.
 
 # Individual constants
-OP_CONT        = 0x00 # Continuation frame
-OP_TEXT        = 0x01 # Text frame
-OP_BINARY      = 0x02 # Binary frame
-OP_CLOSE       = 0x08 # Close frame
-OP_PING        = 0x09 # Ping frame
-OP_PONG        = 0x0A # Pong frame
+OP_CONT   = 0x00 # Continuation frame
+OP_TEXT   = 0x01 # Text frame
+OP_BINARY = 0x02 # Binary frame
+OP_CLOSE  = 0x08 # Close frame
+OP_PING   = 0x09 # Ping frame
+OP_PONG   = 0x0A # Pong frame
 
-OP_MIN         = 0x00 # Minimum possible opcode.
-OP_MAX         = 0x0F # Maximum possible opcode.
+OP_MIN    = 0x00 # Minimum possible opcode.
+OP_MAX    = 0x0F # Maximum possible opcode.
 
 # Bitmasks
 OPMASK_CONTROL = 0x08 # Is the given opcode a control one?
