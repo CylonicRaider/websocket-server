6494784a3d31575e65520b71337a15f7e880a267
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-05-27 15:18:06 +0200

    Migrate to argparse
    It's never too late!

--- websocket_server/quick.py
+++ websocket_server/quick.py
@@ -6,7 +6,7 @@ Convenience functions for quick usage.
 """
 
 import sys
-import optparse
+import argparse
 
 from .server import WebSocketMixIn
 from .httpserver import ThreadingHTTPServer, RoutingRequestHandler
@@ -31,41 +31,37 @@ def run(handler, server=ThreadingHTTPServer, prepare=None, premain=None):
                       to. Constructed from command-line arguments.
             handler : The request handler. Passed through from the same-
                       named argument of run().
-    prepare is a callable that is invoked with the OptionParser (from
-            optparse) instance used to parse options as the only argument.
+    prepare is a callable that is invoked with the ArgumentParser (from
+            argparse) instance used to parse options as the only argument.
             Can be used to specify additional options.
     premain is called immediately before entering the main loop of the
-            internally created server object with three arguments:
+            internally created server object with two arguments:
             httpd    : The server object created; an instance of server.
-            options  : The options as returned by optparse.OptionParser.
-            arguments: The arguments as returned by the latter.
+            arguments: The arguments as returned by argparse.ArgumentParser.
             It can be used to pass on the values of the options configured
             using prepare to the server object and the handler class.
     """
     # Parse command-line arguments.
-    p = optparse.OptionParser()
-    p.add_option('-p', '--port', dest='port', type='int',
-                 default=8080,
-                 help='Specify the port to run on.',
-                 metavar='PORT')
-    p.add_option('-s', '--host', dest='host', default='',
-                 help='Specify the network interface to bind to.',
-                 metavar='IP')
+    p = argparse.ArgumentParser()
+    p.add_argument('--port', '-p', metavar='PORT', type=int, default=8080,
+                   help='Specify the port to run on.')
+    p.add_argument('--host', '-s', metavar='IP', default='',
+                   help='Specify the network interface to bind to.')
     # Call preparation callback
     if prepare: prepare(p)
     # Actually parse arguments.
-    options, arguments = p.parse_args()
+    arguments = p.parse_args()
     # Create server.
-    httpd = server((options.host, options.port), handler)
+    httpd = server((arguments.host, arguments.port), handler)
     # Print status message.
-    if options.host:
+    if arguments.host:
         sys.stderr.write('Serving HTTP on %s:%s...\n' %
-                         (options.host, options.port))
+                         (arguments.host, arguments.port))
     else:
-        sys.stderr.write('Serving HTTP on *:%s...\n' % options.port)
+        sys.stderr.write('Serving HTTP on *:%s...\n' % arguments.port)
     sys.stderr.flush()
     # Call second preparation hook
-    if premain: premain(httpd, options, arguments)
+    if premain: premain(httpd, arguments)
     # Run it.
     try:
         httpd.serve_forever()
