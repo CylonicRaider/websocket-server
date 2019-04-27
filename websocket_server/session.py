b55b040c4b8714b98d22a1b125cac229ff2ad136
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-04-27 23:28:03 +0200

    Commence WebSocketSession implementation

--- websocket_server/session.py
+++ websocket_server/session.py
@@ -11,3 +11,36 @@ endpoint, retrying commands whose results may have been missed, etc.
 
 This module is NYI.
 """
+
+import threading
+
+__all__ = ['WebSocketSession']
+
+class WebSocketSession(object):
+    """
+    WebSocketSession(url) -> new instance
+
+    A "session" spanning multiple WebSocket connections. url is the WebSocket
+    URL to connect to.
+
+    The constructor parameter is stored in a same-named instance variable; it
+    may be modified to change where future connections are going to go.
+    """
+
+    def __init__(self, url):
+        """
+        __init__(url) -> None
+
+        Instance initializer; see the class docstring for details.
+        """
+        self.url = url
+        self.cond = threading.Condition()
+        self._conn = None
+
+    def __enter__(self):
+        "Context manager entry; internal."
+        self.cond.__enter__()
+
+    def __exit__(self, *args):
+        "Context manager exit; internal."
+        self.cond.__exit__(*args)
