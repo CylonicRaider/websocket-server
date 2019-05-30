CylonicRaider/websocket-server

commit 99569843e5a24074aec695af04e24200f1e72295

Implement Future class

websocket_server/tools.py
@@ -588,6 +588,73 @@ class Scheduler(object):
             while self._references > 0:
                 self.wait(None)
 
+class Future(object):
+    """
+    Future(cb) -> new instance
+
+    A wrapper around an object that may have yet to be computed. The
+    computation of the object is represented by the callable cb, which is
+    called with no arguments and returns the computed object.
+
+    Additional read-only instance attributes are:
+    value: The object enclosed by this Future, or None is not computed yet.
+    state: The computation state of this Future (one of the ST_PENDING ->
+           ST_COMPUTING -> ST_DONE constants). Note that reading this is
+           probably of little use since the value might change immediately
+           after accessing it.
+    """
+
+    ST_PENDING   = 'PENDING'
+    ST_COMPUTING = 'COMPUTING'
+    ST_DONE      = 'DONE'
+
+    def __init__(self, cb):
+        """
+        __init__(cb) -> None
+
+        Instance initializer; see the class docstring for details.
+        """
+        self.cb = cb
+        self.value = None
+        self.state = self.ST_PENDING
+        self._cond = threading.Condition()
+
+    def run(self):
+        """
+        run() -> None
+
+        Compute the value wrapped by this Future (if that has not happened
+        yet).
+        """
+        with self._cond:
+            if self._state != self.ST_PENDING: return
+            self._state = self.ST_COMPUTING
+        self.value = self.cb()
+        with self._cond:
+            self._state = self.ST_DONE
+            self._cond.notifyAll()
+
+    def get(self, wait=True, default=None, run=False):
+        """
+        get(wait=True, default=None, run=False) -> object
+
+        Retrieve the object wrapped by this Future. If wait is false, this
+        returns default immediately if the object is not available yet. If
+        compute and wait are true and the object is not available yet, it is
+        computed (in this thread).
+        """
+        with self._cond:
+            if self._state == self.ST_DONE:
+                return self.value
+            elif not wait:
+                return default
+        if compute:
+            self.run()
+        with self._cond:
+            while self._state != self.ST_DONE:
+                self._cond.wait()
+            return self.value
+
 class MutexBarrier(object):
     """
     MutexBarrier() -> new instance

commit cc7c7abd7ad1b727ab138983eabba7dcb8ca27d0

Unify Future state spelling
Bug introduced in commit 99569843e5a24074aec695af04e24200f1e72295, the
initial implementation of Future.

websocket_server/tools.py
@@ -751,13 +751,13 @@ class Future(object):
         callback to run.
         """
         with self._cond:
-            if self._state == self.ST_DONE:
+            if self.state == self.ST_DONE:
                 return True
-            elif self._state == self.ST_COMPUTING:
+            elif self.state == self.ST_COMPUTING:
                 return Ellipsis
             elif self.cb is None:
                 return False
-            self._state = self.ST_COMPUTING
+            self.state = self.ST_COMPUTING
         v = self.cb()
         if not self._set(v, self.ST_COMPUTING):
             raise AssertionError('Future has gotten into an invalid state')
@@ -779,7 +779,7 @@ class Future(object):
              after it has been called.
         """
         with self._cond:
-            return self._PENDING_MAP[self._state]
+            return self._PENDING_MAP[self.state]
 
     def get(self, default=None):
         """
@@ -789,7 +789,7 @@ class Future(object):
         is not available yet.
         """
         with self._cond:
-            return self.value if self._state == self.ST_DONE else default
+            return self.value if self.state == self.ST_DONE else default
 
     def _set(self, value, check_state):
         """
@@ -839,15 +839,15 @@ class Future(object):
         cannot guarantee that the computation will honor the timeout.
         """
         with self._cond:
-            if self._state == self.ST_DONE:
+            if self.state == self.ST_DONE:
                 return self.value
             elif not run or self.cb is None:
                 if timeout is None:
-                    while self._state != self.ST_DONE:
+                    while self.state != self.ST_DONE:
                         self._cond.wait()
                     return self.value
                 deadline = time.time() + timeout
-                while self._state != self.ST_DONE:
+                while self.state != self.ST_DONE:
                     now = time.time()
                     if now >= deadline:
                         raise self.Timeout('Future value did not arrive in '
@@ -858,7 +858,7 @@ class Future(object):
             raise ValueError('Cannot honor timeout while computing value')
         self.run()
         with self._cond:
-            while self._state != self.ST_DONE:
+            while self.state != self.ST_DONE:
                 self._cond.wait()
             return self.value
 
@@ -872,7 +872,7 @@ class Future(object):
         method returns whether the callback was invoked immediately.
         """
         with self._cond:
-            if self._state != self.ST_DONE:
+            if self.state != self.ST_DONE:
                 self.done_cbs.append(cb)
                 return False
             v = self.value